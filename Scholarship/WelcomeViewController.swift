53f67e52f58d2f48ac2628738278e798616877e1
Laurin Brandner <mail@laurinbrandner.ch>
2015-02-07 16:54:02 +0100

    Add label

--- Scholarship/WelcomeViewController.swift
+++ Scholarship/WelcomeViewController.swift
@@ -26,20 +26,37 @@ class WelcomeViewController: UIViewController {
         return button
     }()
     
+    lazy var textLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hi There! \n My name is Laurin. I'm from Switzerland. I code. A lot."
+        label.font = UIFont.systemFontOfSize(24.0)
+        label.backgroundColor = UIColor.greenColor()
+        label.numberOfLines = 0
+        label.textAlignment = .Center
+        
+        return label
+    }()
+    
     // MARK: - View Lifecycle
     
     override func loadView() {
         super.loadView()
         
         self.view.addSubview(self.avatarButton)
-        constrain(self.view, self.avatarButton) { view, avatarButton in
+        self.view.addSubview(self.textLabel)
+        
+        constrain(self.view, self.avatarButton, self.textLabel) { view, avatarButton, textLabel in
             avatarButton.width  == 100
             avatarButton.height == 100
             avatarButton.centerX == view.centerX
             avatarButton.top == view.top+50
+            
+            textLabel.width == view.width*0.6
+            textLabel.height == 300
+            textLabel.top == avatarButton.bottom+50
+            textLabel.centerX == view.centerX
         }
     }
-    
     
     // MARK: -
 
