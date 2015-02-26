77e681d0b4ceafca27b163747134a404a26ba299
Laurin Brandner <mail@laurinbrandner.ch>
2015-02-26 09:03:59 +0100

    Improve texts a bit

--- Scholarship/Models/Topic.swift
+++ Scholarship/Models/Topic.swift
@@ -25,7 +25,7 @@ struct Topic {
     
     static func aboutTopic() -> Topic {
         let aboutMeParagraph = Paragraph(title: NSLocalizedString("About Me", comment: "About Me"),
-                                          text: NSLocalizedString("My name is Laurin Brandner. When my dad bought our first iMac, I was 14 at that time, I became interested in developing Mac apps. With 15 I developed my first Mac and iOS application. Since then there’s scarcely a day I don’t code anymore. By reading blogs and books I have learnt a handful of programming languages including: Objective-C, C, Python, Ruby and Swift. During my last year of high school I was also working part time at Ubique, a software agency based in Zurich.", comment: "About Me Text"))
+                                          text: NSLocalizedString("My name is Laurin Brandner. When my dad bought our first iMac, I was 14 at that time, I became interested in developing Mac apps. With 15 I developed my first Mac and iOS application. Since then there’s scarcely a day I don’t code anymore. By reading blogs and books I have learnt a handful of programming languages including Objective-C, C, Python, Ruby and Swift. During my last year of high school I was also working part time at Ubique, a software agency based in Zurich.", comment: "About Me Text"))
         let educationParagraph = Paragraph(title: NSLocalizedString("Education", comment: "Education"),
                                             text: NSLocalizedString("I graduated from the Alte Kantonsschule Aarau in 2014 and will begin my studies at the Swiss Federal Institute of Technology (ETH) in Zurich this year. I’m planning on pursuing a Master degree in Computer Science.", comment: "Education Text"))
         let futureParagraph = Paragraph(title: NSLocalizedString("Future", comment: "Future"),
@@ -50,7 +50,7 @@ struct Topic {
         
         let whistlesParagraph: Paragraph = {
             var paragraph = Paragraph(title: "Whistles",
-                                       text: NSLocalizedString("Whistles is an iPhone app that identifies birds by their whistling. I developed an audio fingerprinting algorithm that consists of multiple algorithms including the Fast Fourier Transform and the Haar-Wavelet Transform. To store the fingerprints of the birds and match them I have also developed a Ruby on Rails server. I won the \"Best Computer Science Graduation Project of the Year 2014\" award with it.", comment: "Crimson Text"))
+                                       text: NSLocalizedString("Whistles is an iPhone app that identifies birds by their songs. I developed an audio fingerprinting algorithm that consists of multiple analysis techniques including the Fast Fourier Transform and the Haar-Wavelet Transform. To store and match the generated fingerprints I have also developed a Ruby on Rails server. I won the \"Best Computer Science Graduation Project of the Year 2014\" award with it.", comment: "Crimson Text"))
             if let URL = NSURL(string: "https://github.com/larcus94/LBAudioDetective") {
                 paragraph.source = .GitHub(URL)
             }
@@ -68,7 +68,7 @@ struct Topic {
     static func interestsTopic() -> Topic {
         let programmingParagraph: Paragraph = {
             var paragraph = Paragraph(title: NSLocalizedString("Programming", comment: "Programming"),
-                text: NSLocalizedString("Programming is my biggest hobby. I develop iOS and Mac apps for about 6 years. However, I'm also interested in web development using Node.js or Ruby on Rails. Most recently I have also used OpenCV to make a script that takes a picture whenever you wink at the camera. It intrigues me that computers can learn such human like behavior. When I have the time, I also like to go to meetups or attend hackathons.", comment: "Programming Text"))
+                text: NSLocalizedString("Programming is my biggest hobby. I develop iOS and Mac apps for about 6 years. However, I'm also interested in web development using Node.js or Ruby on Rails. Most recently I have also used OpenCV to make a script that takes a picture whenever you wink at the camera. It intrigues me that computers can learn such human like behavior. When I have the time, I also like to go to meetups or participate in hackathons.", comment: "Programming Text"))
             paragraph.images = [(UIImage(named: "Programming-Wink")!, nil),
                 (UIImage(named: "Programming-HackZurich")!, nil)]
             
@@ -77,7 +77,7 @@ struct Topic {
         
         let snowboardingParagraph: Paragraph = {
             var paragraph = Paragraph(title: NSLocalizedString("Snowboarding", comment: "Snowboarding"),
-                text: NSLocalizedString("As a Swiss, I'm obsessed with the mountains. I started skiing when I was 6 years old and learnt snowboarding when I was 12. I try to go snowboarding as many times as possible during the winter. I'm still convinced that there's nothing more fun than a beautiful day with a meter of fresh powder.", comment: "Snowboarding Text"))
+                text: NSLocalizedString("As a Swiss, I'm obsessed with the mountains. I started skiing when I was 6 years old and learnt snowboarding when I was 12. During the winter I try to go snowboarding as many times as possible. I'm still convinced that there's nothing more fun than a beautiful day with a meter of fresh powder.", comment: "Snowboarding Text"))
             paragraph.images = [(UIImage(named: "Snowboarding-Kimberley")!, nil),
                 (UIImage(named: "Snowboarding-Tour")!, nil)]
             
